7acbaf44585b0b4b5fa2644b96bef60896504951
Thomas Witherow <twitherow@bellsouth.net>
2021-06-02 14:47:01 -0500

    listener not listening

--- scripts/RegisteredPets.js
+++ scripts/RegisteredPets.js
@@ -1,6 +1,31 @@
-import { getPets } from "./database.js"
+import { getPets, getWalkers } from "./database.js"
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        if (itemClicked.id.startsWith("pet")) {
+            const [, petId] = itemClicked.id.split("--")
+
+            for (const pet of pets) {
+                if (pet.id === parseInt(petId)) {
+
+                    let assignedWalker = walkers.find(walker => walker.id === pet.walkerId )
+
+                     
+                    
+                            window.alert(`${pet.name} is being walked by ${assignedWalker.name}`)
+                        
+                    
+                }
+
+            }
+        }
+    }
+)
 
 const pets = getPets()
+const walkers = getWalkers()
 
 export const RegisteredPets = () => {
     let petHTML = "<ul>"
