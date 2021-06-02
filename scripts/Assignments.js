5ac1f708d42fde36754279687bcb45917c22b07c
Thomas Witherow <twitherow@bellsouth.net>
2021-06-02 16:14:54 -0500

    yay got it

--- scripts/Assignments.js
+++ scripts/Assignments.js
@@ -1,9 +1,9 @@
-import { getPets, getWalkers } from "./database.js"
+import { getPets, getWalkers, getCities } from "./database.js"
 
 // Get copy of state for use in this module
 const pets = getPets()
 const walkers = getWalkers()
-
+const cities = getCities()
 
 // Function whose responsibility is to find the walker assigned to a pet
 const findWalker = (pets, walkers) => {
@@ -18,20 +18,31 @@ const findWalker = (pets, walkers) => {
     return petWalker
 }
 
+const findCity = (walkers, cities) => {
+    let cityLocation = null
+
+    for (const city of cities) {
+        if (city.id === walkers.cityId) {
+            cityLocation = city
+        }
+    }
+    return cityLocation
+}
+
 export const Assignments = () => {
     let assignmentHTML = ""
       assignmentHTML = "<ul>"
 
     for (const currentPet of pets) {
         const currentPetWalker = findWalker(currentPet, walkers)
-      
+        const currentCityLocation = findCity(currentPetWalker, cities )
         assignmentHTML += `
             <li>
                 ${currentPet.name} is being walked by
-                ${currentPetWalker.name} in ${currentPetWalker.city}
+                ${currentPetWalker.name} in ${currentCityLocation.name}
                 </li>
                 `
-                console.log(currentPet.name, currentPetWalker.name, currentPetWalker.city)
+                console.log(currentPet.name, currentPetWalker.name, currentCityLocation.name)
     }
 
     assignmentHTML += "</ul>"
--- scripts/RegisteredPets.js
+++ scripts/RegisteredPets.js
@@ -10,10 +10,9 @@ document.addEventListener(
             for (const pet of pets) {
                 if (pet.id === parseInt(petId)) {
 
-                    let assignedWalker = walkers.find(walker => walker.id === pet.walkerId )
+                    let assignedWalker = walkers.find(walker => walker.id === pet.walkerId);
 
-                     
-                    
+                   
                             window.alert(`${pet.name} is being walked by ${assignedWalker.name}`)
                         
                     
@@ -31,7 +30,7 @@ export const RegisteredPets = () => {
     let petHTML = "<ul>"
 
     for (const pet of pets) {
-        petHTML += `<li>${pet.name}</li>`
+        petHTML += `<li id="pet--${pet.id}">${pet.name}</li>`
     }
 
     petHTML += "</ul>"
