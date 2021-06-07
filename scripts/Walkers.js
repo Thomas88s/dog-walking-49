ae688db5de90ea83b72c1192919bc734f01ce4da
Thomas Witherow <twitherow@bellsouth.net>
2021-06-07 13:54:55 -0500

    fixed bugs

--- scripts/Walkers.js
+++ scripts/Walkers.js
@@ -1,15 +1,19 @@
 import { getWalkers } from "./database.js"
+import { getCities } from "./database.js"
 
 document.addEventListener(
     "click",
     (clickEvent) => {
         const itemClicked = clickEvent.target
         if (itemClicked.id.startsWith("walker")) {
-            const [,walkerId] = itemClicked.id.split("--")
+            const [,cityId] = itemClicked.id.split("--")
 
             for (const walker of walkers) {
-                if (walker.id === parseInt(walkerId)) {
-                    window.alert(`${walker.name} services ${walker.city}`)
+                if (walker.id === parseInt(cityId)) {
+
+                    let assignedCity = cities.find(city => city.id === walker.cityId)
+
+                    window.alert(`${walker.name} services ${assignedCity.name}`)
                 }
             }
         }
@@ -18,7 +22,7 @@ document.addEventListener(
 
 
 const walkers = getWalkers()
-
+const cities = getCities()
 
 export const Walkers = () => {
     let walkerHTML = "<ul>"
